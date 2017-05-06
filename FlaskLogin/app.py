6b93ca5d780377c0bd20ef5066e53bd8e169ab35
Shraddha-Yeole <shraddhayeole15@gmail.com>
2017-05-05 21:26:36 -0700

    login code with validations

--- FlaskLogin/app.py
+++ FlaskLogin/app.py
@@ -1,26 +1,67 @@
-import os
-from uuid import uuid4
-import pyautogui
 
-
-from flask import Flask, request, render_template, send_from_directory,redirect, url_for, request
-
-
+from flask import Flask, render_template, request,redirect, url_for
+import sqlite3
+import time
 app = Flask(__name__)
 
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-
+@app.route('/checklogin')
+def log():
+  #return render_template('check.html')
+  return "Try more"
+  
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+        username = request.form['username']
+        password = request.form['password']
+        user=username.split("@")
+        conn = sqlite3.connect('/Users/shraddhayeole/Desktop/Sithu/sithuproject/login.sqlite')
+        c = conn.cursor()
+        c.execute('SELECT * FROM login')
+        print "successful"
+        rows = c.fetchall()
+        print rows
+        recordfounf='false'
+        for row in rows:
+            dbUser = row[0]
+            dbPass = row[1]
+            print dbUser
+            print dbPass
+            if username in dbUser:
+                if password in dbPass:
+                    recordfounf='true'
+                    print recordfounf
+                    break
+                        
+        if recordfounf=='false':
+            
+            print "invalid user"
+
+        print user[1]
+        print recordfounf
+        if user[1] == 'sjsu.edu' and recordfounf=='true':
+            return redirect(url_for('secret'))
+            
+            #return redirect(url_for('log'))
         else:
-            return redirect(url_for('home'))
+            error = 'Invalid Credentials. Please try again.'
+            #time.sleep(10)
+            #return redirect(url_for('login'))
     return render_template('login.html', error=error)
+
+@app.route('/secret')
+def secret():
+    return "You have successfully logged in"
     
 
-if __name__ == "__main__":
-    app.run(port=4555, debug=True)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
